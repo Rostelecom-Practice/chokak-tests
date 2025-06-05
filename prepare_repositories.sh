59b67824f8b58c6a5f1a1b3474ccaacd9b6686d6
Vladick <vlad2004ki@gmail.com>
2025-06-05 19:21:35 +0300

    init tests

---
+++ prepare_repositories.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+mkdir -p services
+
+if [ ! -d services/chokak-server ]; then
+    git clone https://github.com/Rostelecom-Practice/chokak-server.git services/chokak-server
+fi
+
+if [ ! -d services/chokak-gateway-server ]; then
+    git clone https://github.com/Rostelecom-Practice/chokak-gateway-server.git services/chokak-gateway-server
+fi
+
+if [ ! -d services/chokak-cloud-storage ]; then
+    git clone https://github.com/Rostelecom-Practice/chokak-cloud-storage.git services/chokak-cloud-storage
+fi
---
+++ tests/conftest.py
@@ -0,0 +1,27 @@
+import requests
+import os
+import pytest
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+@pytest.fixture(scope="session")
+def firebase_test_id_token():
+    api_key = os.getenv("FIREBASE_API_KEY")
+    email = os.getenv("FIREBASE_TEST_EMAIL")
+    password = os.getenv("FIREBASE_TEST_PASSWORD")
+
+    url = f"https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key={api_key}"
+    payload = {
+        "email": email,
+        "password": password,
+        "returnSecureToken": True
+    }
+
+    response = requests.post(url, json=payload)
+    response.raise_for_status()
+
+    id_token = response.json()["idToken"]
+    print(id_token)
+    return id_token
